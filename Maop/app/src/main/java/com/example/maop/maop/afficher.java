f6907ebbdd95c30d768329bd92df55a6c69f4dcc
Nickying <nicky_kong_91@hotmail.fr>
2016-01-19 16:46:03 +0100

    ajout de la liste

--- Maop/app/src/main/java/com/example/maop/maop/afficher.java
+++ Maop/app/src/main/java/com/example/maop/maop/afficher.java
@@ -3,7 +3,21 @@ package com.example.maop.maop;
 /**
  * Created by Nicky on 19/01/2016.
  */
-public class afficher {
+public class Afficher{
+    private String tab[];
+
+    public Afficher(){
+        tab = new String[6];
+        tab[0]="Nicky";
+        tab[1]="Vang";
+        tab[2]="Guillaume";
+        tab[3]="Goubin";
+        tab[4]="Drexchel";
+        tab[5]="Astom";
+    }
 
 
+    public String getTab(int i){
+        return this.tab[i];
+    }
 }
--- Maop/app/src/main/java/com/example/maop/maop/maListe.java
+++ Maop/app/src/main/java/com/example/maop/maop/maListe.java
@@ -3,12 +3,19 @@ package com.example.maop.maop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class maListe extends AppCompatActivity implements View.OnClickListener{
 
     final String magasin = "magasin";
+
+    private ListView liste;
+    private String[] tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,41 @@ public class maListe extends AppCompatActivity implements View.OnClickListener{
         Button Btnquitter = (Button) findViewById(R.id.btnquitter);
         Button Btnajout = (Button) findViewById(R.id.btnajout);
         Button Btnmodif = (Button) findViewById(R.id.btnmodif);
+        liste = (ListView) findViewById(R.id.listProduit);
 
         Btnquitter.setOnClickListener(this);
         Btnajout.setOnClickListener(this);
         Btnmodif.setOnClickListener(this);
+
+        Afficher afficher = new Afficher();
+
+        affiche(afficher);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tab);
+        liste.setAdapter(adapter);
+
+         liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) liste.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+
     }
 
     public void onClick(View v){
@@ -37,5 +75,14 @@ public class maListe extends AppCompatActivity implements View.OnClickListener{
 
     private void quitter(){
         finish();
+    }
+
+    private void affiche(Afficher afficher){
+
+        tab = new String[6];
+        for(int i=0;i<tab.length;i++){
+            tab[i] = afficher.getTab(i);
+            System.out.println(afficher.getTab(i));
+        }
     }
 }
